9ae692f39da53e6ca7074fa95cf36dbc3bbaf59b
Fgunja <fran.gunja@gmail.com>
2024-12-05 10:27:38 +0100

    commit changes

---
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+  <h1> Provjera ishoda 1 i 2 </h1>
+  </div>
+  </q-page>
+  
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
